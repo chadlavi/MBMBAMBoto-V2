f88b207ce41da4d062e1dbe7144c513a51378de9
Zack <zack.baker@lab.kingsu.ca>
2016-03-23 18:39:17 -0600

    Started project; added files

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a reddit bot which will fetch links to episodes of the [My Brother, My Brother and Me](http://www.maximumfun.org/shows/my-brother-my-brother-and-me) podcast. By listening and parsing comments on the [/r/mbmbam](http://mbmbam.reddit.com) subreddit, it will comment a link to a requested episode. The bot listens to each comment, attempting to match the regular expression "\!\d+". For each match, it uses the digit string to connect to the associated episode based on the shows rss feed.   
+**EXAMPLE**  
+Say someone comments "!100". The bot matches "!100" with the regular expression, and gets the episode link for episode 100. It then processes this link into a string to comment.
---
+++ mbmbamboto.py
@@ -0,0 +1,108 @@
+import praw
+import time
+import urllib
+import OAuth2Util
+import feedparser
+import re
+#Special eps: The Adventure Zone:	TAZ				213
+#			  Tostinos: 			Tostinos 		262
+#			  Switcharoo: 			Switch 			273
+def is_int(n):
+	try:
+		int(n)
+		return True
+	except ValueError:
+		return False
+
+def is_empty_string(q):
+	if q is "":
+		return False
+	else:
+		return True
+def get_main_list(rss_obj):
+	item_lst = rss_obj["items"]
+	main_ep_lst = []
+	for i in item_lst:
+		if len(re.findall("Me:|\d+:",i["title"])) > 0:
+			main_ep_lst.append(i)
+	return main_ep_lst
+
+r = praw.Reddit("My Brother, My Brother, and Me Episode Grabber v1.0 by /u/Quip_Qwop") # Create the reddit object
+o = OAuth2Util.OAuth2Util(r,print_log=True) #OAuth verification stuff
+o.refresh(force=True) #OAuth token refresh stuff; Always auto-refresh token when neccesary
+while True:
+	try:
+		print "Beginning of Loop"
+		id_file = open("idfile","r+") #open the idfile
+		id_file_string = id_file.read()# store the contents of idfile into a large string 
+		id_file_list = id_file_string.split("\n") #split the string across newlines and store into an array
+		old_len = len(id_file_list) #get the number of processed ids
+		full_comments = r.get_subreddit("mbmbam").get_comments() #get the comments generator
+		for comment in full_comments: #for each comment in the generator
+			if str(comment.id) not in id_file_list: #if thc comments id isn't in the list of processed ids; i.e. Check if the current comment has been processed or not
+				id_file_list.append(str(comment.id)) #store this comments id in the list of processed ids
+				print "Added " + comment.id + " to the list of processed ids"
+				digit_list = [] #create a list to store the numbers found in the comment
+				#comment_body_list = comment.body.split(" ")#split the body of the comment across spaces to separate it into a list of words
+				reply_str = "" # prepare a string to hold the reply
+				match_list = re.findall("\!-?\d+|\!recent|\!TAZ|\!Tostino|\!Switch|\!noadvice",comment.body) 
+				for match in match_list: # for each word in the comment
+					if is_int(match[1:]):# if the match iis a number
+						digit_list.append(str(abs(int(match[1:])))) # add the number to the list of episode numbers
+					elif match=="!recent":
+						feed = get_main_list(feedparser.parse("http://mbmbam.libsyn.com/rss"))
+						digit_list.append(len(feed))
+					elif match=="!TAZ":
+						digit_list.append(-1)
+					elif match=="!Switch":
+						digit_list.append(-2)
+					elif match=="!Tostino":
+						digit_list.append(-3)
+					elif match=="!noadvice":
+						digit_list.append(259)
+
+				if len(digit_list)>0: # if theres at least one link request
+					feed = get_main_list(feedparser.parse("http://mbmbam.libsyn.com/rss")) # parse the rss feed
+					rv_list = feed[::-1] #reverse the list of items, for easier navigation
+					for ep in digit_list: # for each episode number in the comment
+						if ep==-3:
+							real_list=feedparser.parse("http://mbmbam.libsyn.com/rss")["items"][::-1]
+							reply_str+="["+real_list[262]["title"]+"]("+real_list[262]["link"]+")\n\n  "
+						elif ep==-2:
+							real_list=feedparser.parse("http://mbmbam.libsyn.com/rss")["items"][::-1]
+							reply_str+="["+real_list[273]["title"]+"]("+real_list[273]["link"]+")\n\n  "
+						elif ep==-1:
+							real_list=feedparser.parse("http://mbmbam.libsyn.com/rss")["items"][::-1]
+							reply_str+="["+real_list[213]["title"]+"]("+real_list[213]["link"]+")\n\n  "
+						else:
+							try:
+								reply_str+="[" + rv_list[abs(int(ep))-1]["title"] + "](" + rv_list[abs(int(ep))-1]["link"]+")\n\n " # try to get the ep-1th index of the list
+							except IndexError: # if it doesn't exist
+								reply_str+="Episode " + str(ep) + " doesn't exist!\n\n " # comment that the episode doesn't exist yet
+				print reply_str	# print the full reply
+				print "~~~~~~~~~~~~~\n" + comment.body + "\n~~~~~~~~~~~~~\n" #print the comment body
+				if len(reply_str)>0: # if the reply string has content
+					comment.reply(reply_str) #reply to the comment with the reply string
+		id_file.seek(0)# go to the top of the file
+
+		new_len = len(id_file_list) #get the length of the id list after potentially adding new ids
+		if (new_len-old_len)>0: # if there are new ids, rewrite the idfile
+			for id_num in id_file_list:
+				id_file.write(id_num + "\n")
+		id_file.close()#close the idfile
+		
+	except (Exception, RuntimeError) as e:
+		now = time.strftime("%c")
+		err_log = open("errlog","r+")
+		err_log.write("!!!\nSomething went wrong at :" + now + "\n")
+		err_log.write(str(type(e)))
+		err_log.write("\n!!!\n\n")
+		err_log.close()
+		print "something went wrong:\n", type(e),"\n",e
+	else:
+		time.sleep(5) #sleep for 5 seconds
+
+
+
+
+
