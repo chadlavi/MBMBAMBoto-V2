5290d151215b7659ddd557a3f72cabc8bfb10e71
root <root@mbmbamboto.localdomain>
2017-03-31 22:08:51 +0000

    first working praw setup

--- mbmbamboto.py
+++ mbmbamboto.py
@@ -1,12 +1,13 @@
+import os, sys
 import praw
 import time
 import urllib
-import OAuth2Util
 import feedparser
 import re
 #Special eps: The Adventure Zone:	TAZ				213
 #			  Tostinos: 			Tostinos 		262
 #			  Switcharoo: 			Switch 			273
+episode_pattern = re.compile(r"[eE]p(?<=isode)?\ (?<=#)?(\d+)|(\!recent)|(\!TAZ)|(\!Tostino)|(\!Switch)|(\!noadvice)")
 def is_int(n):
 	try:
 		int(n)
@@ -27,17 +28,16 @@ def get_main_list(rss_obj):
 			main_ep_lst.append(i)
 	return main_ep_lst
 
-r = praw.Reddit("My Brother, My Brother, and Me Episode Grabber v1.0 by /u/Quip_Qwop") # Create the reddit object
-o = OAuth2Util.OAuth2Util(r,print_log=True) #OAuth verification stuff
-o.refresh(force=True) #OAuth token refresh stuff; Always auto-refresh token when neccesary
+r = praw.Reddit('bot') # Create the reddit object
 while True:
 	try:
 		print "Beginning of Loop"
+		print(r.user.me())
 		id_file = open("idfile","r+") #open the idfile
 		id_file_string = id_file.read()# store the contents of idfile into a large string 
 		id_file_list = id_file_string.split("\n") #split the string across newlines and store into an array
 		old_len = len(id_file_list) #get the number of processed ids
-		full_comments = r.get_subreddit("mbmbam").get_comments() #get the comments generator
+		full_comments = r.subreddit('mbmbam').stream.comments() #get the comments generator
 		for comment in full_comments: #for each comment in the generator
 			if str(comment.id) not in id_file_list: #if thc comments id isn't in the list of processed ids; i.e. Check if the current comment has been processed or not
 				id_file_list.append(str(comment.id)) #store this comments id in the list of processed ids
@@ -45,21 +45,24 @@ while True:
 				digit_list = [] #create a list to store the numbers found in the comment
 				#comment_body_list = comment.body.split(" ")#split the body of the comment across spaces to separate it into a list of words
 				reply_str = "" # prepare a string to hold the reply
-				match_list = re.findall("[eE]p(?<=isode|.)?\ (?<=#)?(\d+)|(\!recent)|(\!TAZ)|(\!Tostino)|(\!Switch)|(\!noadvice)",comment.body).group() 
+				match_list = episode_pattern.findall(comment.body)
+				print match_list
 				for match in match_list: # for each word in the comment
-					if is_int(match[1:]):# if the match is a number
-						digit_list.append(str(abs(int(match[1:])))) # add the number to the list of episode numbers
-					elif match=="!recent":
-						feed = get_main_list(feedparser.parse("http://mbmbam.libsyn.com/rss"))
-						digit_list.append(len(feed))
-					elif match=="!TAZ":
-						digit_list.append(-1)
-					elif match=="!Switch":
-						digit_list.append(-2)
-					elif match=="!Tostino":
-						digit_list.append(-3)
-					elif match=="!noadvice":
-						digit_list.append(259)
+					print match
+					for result in match:
+						if is_int(result):# if the match is a number
+							digit_list.append(str(abs(int(result)))) # add the number to the list of episode numbers
+						elif match=="!recent":
+							feed = get_main_list(feedparser.parse("http://mbmbam.libsyn.com/rss"))
+							digit_list.append(len(feed))
+						elif match=="!TAZ":
+							digit_list.append(-1)
+						elif match=="!Switch":
+							digit_list.append(-2)
+						elif match=="!Tostino":
+							digit_list.append(-3)
+						elif match=="!noadvice":
+							digit_list.append(259)
 
 				if len(digit_list)>0: # if theres at least one link request
 					feed = get_main_list(feedparser.parse("http://mbmbam.libsyn.com/rss")) # parse the rss feed
@@ -93,11 +96,14 @@ while True:
 		
 	except (Exception, RuntimeError) as e:
 		now = time.strftime("%c")
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
 		err_log = open("errlog","r+")
 		err_log.write("!!!\nSomething went wrong at: " + now + "\n")
 		err_log.write(str(type(e)))
 		err_log.write("\n!!!\n\n")
 		err_log.close()
-		print "something went wrong:\n", type(e),"\n",e
+		print "something went wrong:\n", type(e),"\n",e,"\n\n",exc_type, fname, exc_tb.tb_lineno
+		break
 	else:
 		time.sleep(5) #sleep for 5 seconds
