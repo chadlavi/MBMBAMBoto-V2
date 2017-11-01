5a636defffe99204cc82ab5d23c5c61ffb259c99
Chad Lavimoniere <chad@chadlavimoniere.com>
2017-11-01 05:22:00 +0000

    whoops, new version of PRAW came out, and it stopped allowing for [class].permalink method to take the '(fast=True)' parameter.

--- mbmbamboto.py
+++ mbmbamboto.py
@@ -1,9 +1,9 @@
 # SUBREDDIT-SPECIFIC VARIABLES
-subreddit='mbmbam'
+subreddit = 'mbmbam'
 
 from datetime import datetime
 import os, sys, praw, time, urllib, feedparser, re, logging
-logging.basicConfig(filename='logfile', format='%(asctime)s [%(levelname)s] %(message)s', level=logging.INFO)
+logging.basicConfig(filename = 'logfile', format = '%(asctime)s [%(levelname)s] %(message)s', level = logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler())
 
 episode_pattern = re.compile(r"[eE][pP].?\ #?(\d+)|[eE]pisode\ #?(\d+)|\!(\d+)|(\!latest)|(\!last)|(\!recent)|(![tT]roll)|([tT]rolls?\ 2)|(\!TAZ)|(\!Tostino)|(\!Switch)|(\!noadvice)")
@@ -37,90 +37,90 @@ def get_numbered_eps():
     item_lst = get_all_eps()
     main_ep_lst = []
     for i in item_lst:
-        if len(re.findall("Me:|\d+:", i["title"])) > 0:
+        if len(re.findall(r"Me:|\d+:", i["title"])) > 0:
             main_ep_lst.append(i)
     return main_ep_lst
 
 def pick_ep(result):
     logging.info("result: {0}".format(str(result)))
     if is_int(result):
-        digit_list.append(str(abs(int(result)))) 
+        digit_list.append(str(abs(int(result))))
     elif result in ("!recent", "!last", "!latest"):
         feed = get_all_eps()
         digit_list.append('#'+str(len(feed)))
-    elif result=="!TAZ":
+    elif result == "!TAZ":
         digit_list.append('taz')
-    elif result=="!Switch":
+    elif result == "!Switch":
         digit_list.append('switch')
-    elif result=="!Tostino":
+    elif result == "!Tostino":
         digit_list.append('tostino')
-    elif result=="!noadvice":
+    elif result == "!noadvice":
         digit_list.append(259)
-    elif re.search("\!?[tT]roll", result):
+    elif re.search(r"\!?[tT]roll", result):
         digit_list.append('troll')
 
-r = praw.Reddit('bot') 
+r = praw.Reddit('bot')
 logging.info("Signed in as {0}".format(str(r.user.me())))
 while True:
     try:
         logging.info("Beginning to listen for new comments")
-        with open('idfile', 'r+') as id_file: 
+        with open('idfile', 'r+') as id_file:
             id_file_string = id_file.read()
-        id_file_list = id_file_string.split("\n") 
+        id_file_list = id_file_string.split("\n")
         full_comments = r.subreddit(subreddit).stream.comments()
-        for comment in full_comments: 
+        for comment in full_comments:
             body = comment.body.encode('ascii', 'ignore')
-            if str(comment.id) not in id_file_list and str(comment.author) != 'mbmbamboto': 
-                with open('idfile', 'a+') as id_file: 
+            if str(comment.id) not in id_file_list and str(comment.author) != 'mbmbamboto':
+                with open('idfile', 'a+') as id_file:
                     id_file.write(str(comment.id)+"\n")
-                digit_list = [] 
-                reply_str = "" 
+                digit_list = []
+                reply_str = ""
                 match_list = episode_pattern.findall(body)
                 if len(match_list) > 0:
                     comment.upvote()
                     logging.info("\n\n~~~~~~~~~~~~~\n")
                     logging.info("comment {0}: \"{1}\"".format(str(comment.id), str(body)))
-                    logging.info("comment permalink: https://www.reddit.com/r/{0}{1}".format(subreddit, str(comment.permalink(fast=True))))
-                for match in match_list: 
+                    logging.info("comment permalink: https://www.reddit.com/r/%s%s", str(subreddit), str(comment.permalink))
+                for match in match_list:
                     if type(match) == tuple:
                         for result in match:
                             if len(result) > 0:
-                                pick_ep(result)         
+                                pick_ep(result)        
                     else:
                         pick_ep(match)
-                if len(digit_list)>0: 
-                    rv_list = get_numbered_eps() 
-                    for ep in digit_list: 
+                if len(digit_list)>0:
+                    rv_list = get_numbered_eps()
+                    for ep in digit_list:
                         logging.info("Matching episode: {0}".format(str(ep)))
-                        if ep=='troll':
+                        if ep == 'troll':
                             real_list = get_all_eps()
-                            reply_str+="["+real_list[351]["title"]+"]("+real_list[351]["link"]+")\n\n  "
-                        elif ep=='tostino':
+                            reply_str += "["+real_list[351]["title"]+"]("+real_list[351]["link"]+")\n\n  "
+                        elif ep == 'tostino':
                             real_list = get_all_eps()
-                            reply_str+="["+real_list[262]["title"]+"]("+real_list[262]["link"]+")\n\n  "
-                        elif ep=='switch':
+                            reply_str += "["+real_list[262]["title"]+"]("+real_list[262]["link"]+")\n\n  "
+                        elif ep == 'switch':
                             real_list = get_all_eps()
-                            reply_str+="["+real_list[273]["title"]+"]("+real_list[273]["link"]+")\n\n  "
-                        elif ep=='taz':
+                            reply_str += "["+real_list[273]["title"]+"]("+real_list[273]["link"]+")\n\n  "
+                        elif ep == 'taz':
                             real_list = get_all_eps()
-                            reply_str+="["+real_list[213]["title"]+"]("+real_list[213]["link"]+")\n\n  "
+                            reply_str += "["+real_list[213]["title"]+"]("+real_list[213]["link"]+")\n\n  "
                         elif re.search("#",ep):
-                            ep=int(ep.replace('#',''))-1
+                            ep = int(ep.replace('#',''))-1
                             real_list = get_all_eps()
-                            reply_str+="["+real_list[ep]["title"]+"]("+real_list[ep]["link"]+")\n\n  "
+                            reply_str += "["+real_list[ep]["title"]+"]("+real_list[ep]["link"]+")\n\n  "
                         else:
                             try:
                                 if int(ep) == 420 or int(ep) == 69:
-                                    reply_str+="Nice. [" + rv_list[abs(int(ep))-1]["title"] + "](" + rv_list[abs(int(ep))-1]["link"]+")\n\n " 
+                                    reply_str += "Nice. [" + rv_list[abs(int(ep))-1]["title"] + "](" + rv_list[abs(int(ep))-1]["link"]+")\n\n "
                                 else:
-                                    reply_str+="[" + rv_list[abs(int(ep))-1]["title"] + "](" + rv_list[abs(int(ep))-1]["link"]+")\n\n " 
-                            except IndexError: 
-                                reply_str+="Episode " + str(ep) + " doesn't exist!\n\n " 
-                if len(reply_str)>0: 
-                    reply_str+="-\n\n*I'm a bot. For more details see [this thread](https://www.reddit.com/r/MBMBAM/comments/62qi9c/reminder_you_can_use_the_mbmbamboto_to_quickly/).*"
+                                    reply_str += "[" + rv_list[abs(int(ep))-1]["title"] + "](" + rv_list[abs(int(ep))-1]["link"]+")\n\n "
+                            except IndexError:
+                                reply_str += "Episode " + str(ep) + " doesn't exist!\n\n "
+                if len(reply_str)>0:
+                    reply_str += "-\n\n*I'm a bot. For more details see [this thread](https://www.reddit.com/r/MBMBAM/comments/62qi9c/reminder_you_can_use_the_mbmbamboto_to_quickly/).*"
                     logging.info("my reply:\n{0}".format(str(reply_str)))
-                    comment.reply(reply_str) 
-                    time.sleep(5) 
+                    comment.reply(reply_str)
+                    time.sleep(5)
     except (Exception, RuntimeError) as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -128,5 +128,5 @@ while True:
             err_log.write("Error at " + timestamp() + ":\n")
             err_log.write(str(type(e))+"\n"+str(e)+"\n"+str(exc_type)+"\nfile: "+str(fname)+"\nline number: "+str(exc_tb.tb_lineno))
             err_log.write("\n\n----------\n")
-        logging.error("Something went wrong:\n{1}\n{2}\n{3}\nfile: {4}\nline number: {5}".format(str(timestamp()), str(type(e)), "\n", str(e), "\n", str(exc_type), "\nfile: ", str(fname), "\nline number: ", str(exc_tb.tb_lineno)))
+        logging.error("Something went wrong:\n{}\n{}\n{}\n{}\nfile: {}\nline number: {}".format(str(timestamp()), str(type(e)), str(e), str(exc_type), str(fname), str(exc_tb.tb_lineno)))
         break
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-until `python mbmbamboto.py`; do
+until `python2 mbmbamboto.py`; do
     echo "'mbmbamboto.py' crashed with exit code $?. Restarting..." >&2
     sleep 1
 done
